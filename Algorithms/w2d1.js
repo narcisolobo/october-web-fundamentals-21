ca0664d9770d942bb0615f6904a47d10b5b82236
narcisolobo <narcisolobo@gmail.com>
2021-10-18 10:47:02 -0500

    added w2d1 algo

---
+++ Algorithms/w2d1.js
@@ -0,0 +1,71 @@
+// var floor = Math.floor(1.8);
+// var ceiling = Math.ceil(Math.PI);
+
+// Let's put Math.random in a for loop, just to see what it does.
+for (let i = 0; i < 100; i++) {
+  var random = Math.random();
+  console.log(random);
+}
+// Wow, cool!
+// Math.random gives us a decimal number between zero and one (non-inclusive).
+
+// console.log(floor);
+// console.log(ceiling);
+
+// Dice Roller
+// Using what we've learned about the Math library in JavaScript, complete the following function that should return a value between 1 through 6 at random.
+
+function d6() {
+  // create a variable and assign a random decimal number between 0 and 1 to it.
+  var roll = Math.random();
+  // multiply by six to change the range. now we get a decimal number between 0 and 5.
+  roll = roll * 6;
+  // add one to roll, so our range is now a decimal number from 1 to 6.
+  roll = roll + 1;
+  // chop off that decimal number with Math.floor
+  roll = Math.floor(roll);
+  // all done! return our result.
+  return roll;
+}
+
+var playerRoll = d6();
+console.log("The player rolled: " + playerRoll);
+
+// Consult the Oracle
+// Using the following array, write a function that will answer all of our questions by randomly choosing a response
+
+function magic8Ball() {
+  var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+  ];
+
+  // let's create an index variable that's a random decimal number between 0 and the length of the lifesAnswers array.
+  var index = Math.random() * lifesAnswers.length;
+  // chop off that decimal on our index.
+  index = Math.floor(index);
+  // return a random answer
+  return lifesAnswers[index];
+}
+
+// Hey, magic eightball, will I pass the yellow belt exam?
+var reply = magic8Ball();
+console.log(reply)
